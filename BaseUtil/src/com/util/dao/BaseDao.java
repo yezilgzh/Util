06c1de479b48d1f20369d8d054f042a2c3eae0ca
anonymous <471697737@qq.com>
2014-11-14 09:23:28 +0800

    重新提交

--- BaseUtil/src/com/util/dao/BaseDao.java
+++ BaseUtil/src/com/util/dao/BaseDao.java
@@ -5,14 +5,31 @@ import java.lang.reflect.Type;
 
 public class BaseDao<T> {
 	private Class entityClass;
-
-	// private HibernateTemplate template;
+	@Autowired
+	private HibernateTemplate template;
 
 	public BaseDao() {
 		Type type = getClass().getGenericSuperclass();
 		Type[] types = ((ParameterizedType) type).getActualTypeArguments();
 		entityClass = (Class) types[0];
 
+	}
+
+	public T get(String id) {
+		return (T) template.get(entityClass, id);
+
+	}
+
+	public void save(T entity) {
+		template.save(entity);
+	}
+
+	public void update(T entity) {
+		template.update(entity);
+	}
+
+	public HibernateTemplate getTemplate() {
+		return template;
 	}
 
 }
